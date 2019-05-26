d95498798bc38695e5b2cec00d10f3b768286912
vince <vince@DESKTOP-U73D2FM>
2019-05-27 01:34:29 +0800

    CL-2019.1.3 <vince@DESKTOP-U73D2FM Create ide.general.xml, _@user_Default.icls, project.default.xml, laf.xml, Default.xml, cpp.toolchains.xml, find.xml, ui.lnf.xml, _@user_Xcode.icls, wsl.distributions.xml, editor.codeinsight.xml, colors.scheme.xml, editor.xml, Unnamed.cpp, debugger.xml, updates.xml

---
+++ fileTemplates/Unnamed.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+//#pragma warning( disable : 4996 )
+#define ll long long
+#define pb push_back
+#define endl '\n'
+#define fastIO cin.tie( 0 );ios_base::sync_with_stdio( false )
+#define pii pair<int, int>
+#define eps 1e-13
+#define N 14
+
+using namespace std;
+
+inline void init()
+{
+
+}
+
+inline void solve()
+{
+
+}
+
+int main()
+{
+    fastIO;
+    init();
+    solve();
+
+    return 0;
+}
